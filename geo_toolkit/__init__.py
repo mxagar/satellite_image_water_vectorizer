4728e33158f9db1e82be4803e2d0349cb5a56c3b
Mikel <mxagar@gmail.com>
2023-03-27 17:16:34 +0200

    feat: import all functions

--- geo_toolkit/__init__.py
+++ geo_toolkit/__init__.py
@@ -1,1 +1,14 @@
+from .geo_library import (
+    logger,
+    resample_persist_band,
+    resample_bands,
+    crop_persist_band,
+    crop_bands,
+    load_band_image,
+    load_bands,
+    compute_ndvi,
+    compute_ndwi,
+    generate_persist_ndmap    
+)
+
 __version__ = "0.1.0"
