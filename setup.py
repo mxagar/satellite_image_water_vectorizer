0c1ebfa5bcf09631dcbb1b1c92eda5bd81abe972
Mikel <mxagar@gmail.com>
2023-03-27 12:16:28 +0200

    chore: packe setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="geo_toolkit",
+    version="0.1.0",
+    description="A toolkit for geodata processing.",
+    author="Mikel Sagardia",
+    packages=['geo_toolkit'], # folder names
+	zip_safe=False
+)
